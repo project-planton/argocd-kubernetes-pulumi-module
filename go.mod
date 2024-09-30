a2839d26de9ce1399b8180a9bdca1b5c031a6b3b
Swarup Donepudi <swarup.donepudi@gmail.com>
2024-09-30 21:01:32 +0530

    using project-planton apis

--- go.mod
+++ go.mod
@@ -1,8 +1,6 @@
 module github.com/plantoncloud/argocd-kubernetes-pulumi-module
 
-go 1.23.0
-
-toolchain go1.23.1
+go 1.23.1
 
 //replace github.com/plantoncloud/project-planton => ../../plantoncloud/project-planton
 
@@ -16,8 +14,8 @@ require (
 )
 
 require (
-	github.com/plantoncloud/project-planton/apis v0.0.0-20240928044211-9168d6c7a46b
-	github.com/plantoncloud/pulumi-module-golang-commons v0.0.0-20240928073143-7b29b143661c
+	buf.build/gen/go/plantoncloud/project-planton/protocolbuffers/go v1.34.2-20240930080942-f846802b359d.2
+	github.com/plantoncloud/pulumi-module-golang-commons v0.0.0-20240930122638-5bcefac53812
 )
 
 require (
--- main.go
+++ main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
+	argocdkubernetesv1 "buf.build/gen/go/plantoncloud/project-planton/protocolbuffers/go/project/planton/apis/provider/kubernetes/argocdkubernetes/v1"
 	"github.com/pkg/errors"
 	"github.com/plantoncloud/argocd-kubernetes-pulumi-module/pkg"
-	"github.com/plantoncloud/project-planton/apis/zzgo/cloud/planton/apis/code2cloud/v1/kubernetes/argocdkubernetes"
 	"github.com/plantoncloud/pulumi-module-golang-commons/pkg/stackinput"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		stackInput := &argocdkubernetes.ArgocdKubernetesStackInput{}
+		stackInput := &argocdkubernetesv1.ArgocdKubernetesStackInput{}
 
 		if err := stackinput.LoadStackInput(ctx, stackInput); err != nil {
 			return errors.Wrap(err, "failed to load stack-input")
